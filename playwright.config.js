63c9ba228ddc2c57c9ed39c1bf538108aeca0cbb
lilianzc <110060367+lilianzc@users.noreply.github.com>
2026-06-11 18:05:05 -0400

    change playwright config from testDir: './tests', to testDir: '.'

--- playwright.config.js
+++ playwright.config.js
@@ -13,7 +13,8 @@ import { defineConfig, devices } from '@playwright/test';
  * @see https://playwright.dev/docs/test-configuration
  */
 export default defineConfig({
-  testDir: './tests',
+  //'./tests'
+  testDir: '.',
   /* Run tests in files in parallel */
 
   globalSetup: './globalSetup.js',
